fb24a521e03c13c340aa126611837660a9007252
Alexander Wagner <alexander.wagner@desy.de>
2016-07-25 13:16:49 +0200

    Checkin.pm: code cleanup

--- ILS/Transaction/Checkin.pm
+++ ILS/Transaction/Checkin.pm
@@ -1,17 +1,17 @@
 #
 # Copyright (C) 2006-2008  Georgia Public Library Service
-# 
+#
 # Author: David J. Fiander
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of version 2 of the GNU General Public
 # License as published by the Free Software Foundation.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public
 # License along with this program; if not, write to the Free
 # Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
@@ -47,37 +47,35 @@ END
 
 
 sub new {
-    my $class = shift;;
-    my $self = $class->SUPER::new();
+	my $class = shift;;
+	my $self = $class->SUPER::new();
 
-    foreach my $element (keys %fields) {
+	foreach my $element (keys %fields) {
 		$self->{_permitted}->{$element} = $fields{$element};
-    }
+	}
 
-    @{$self}{keys %fields} = values %fields;
+	@{$self}{keys %fields} = values %fields;
 
-    return bless $self, $class;
+	return bless $self, $class;
 }
 
 sub do_checkin {
 	my $self = shift;
 	my $current_loc = shift;
 	my $sc_return_date = shift; # Note, this is SC format
-	$sc_return_date =~  /(\d\d\d\d)(\d\d)(\d\d)    (\d\d)(\d\d)(\d\d)/;
+	$sc_return_date =~	/(\d\d\d\d)(\d\d)(\d\d)    (\d\d)(\d\d)(\d\d)/;
 	my $return_date = "$1-$2-$3"; # INVENIO needs YYYY-MM-DD
-	
+
 	my $barcode = $self->{item}->id;
 	checkin($barcode, $return_date);
-    syslog("LOG_DEBUG", "ILS::Transaction::Checkin: Item %s returned at %s",
-           $barcode, $return_date);
+	syslog("LOG_DEBUG", "ILS::Transaction::Checkin: Item %s returned at %s",
+		$barcode, $return_date);
 }
 
-
-
 sub resensitize {
-    my $self = shift;
+	my $self = shift;
 
-    return !$self->{item}->magnetic;
+	return !$self->{item}->magnetic;
 }
 
 1;
